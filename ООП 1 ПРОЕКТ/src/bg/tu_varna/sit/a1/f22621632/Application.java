3434c03a892f52ae509a864de4cb00250f4feb2f
zxvri23 <161262364+zxvri23@users.noreply.github.com>
2024-03-24 16:01:32 +0200

    Add files via upload

---
+++ ООП 1 ПРОЕКТ/src/bg/tu_varna/sit/a1/f22621632/Application.java
@@ -0,0 +1,7 @@
+package bg.tu_varna.sit.a1.f22621632;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ООП 1 ПРОЕКТ/src/bg/tu_varna/sit/a1/f22621632/modules/Room.java
@@ -0,0 +1,51 @@
+package bg.tu_varna.sit.a1.f22621632.modules;
+
+public class Room {
+    private String roomNumber;
+    private int beds;
+    private boolean isAvailable;
+    private int guests;
+    private String note;
+
+    public Room(String roomNumber, int beds, boolean isAvailable) {
+        this.roomNumber = roomNumber;
+        this.beds = beds;
+        this.isAvailable = true;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public int getGuests() {
+        return guests;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomNumber='" + roomNumber + '\'' +
+                ", beds=" + beds +
+                ", isAvailable=" + isAvailable +
+                ", guests=" + guests +
+                ", note='" + note + '\'' +
+                '}';
+    }
+
+    public void checkInRoom(){
+        
+    }
+}
+
