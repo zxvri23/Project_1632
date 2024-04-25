4d91d5853a5af1b69b603e2a69ff537ec78ded5f
zxvri23 <161262364+zxvri23@users.noreply.github.com>
2024-04-25 22:42:35 +0300

    Updating whole project

---
+++ Application.java
@@ -0,0 +1,11 @@
+package bg.tu_varna.sit.a1.f22621632.Project_1632;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ main_modules/HotelManagementSystem.java
@@ -0,0 +1,19 @@
+package bg.tu_varna.sit.a1.f22621632.Project_1632.main_modules;
+
+import bg.tu_varna.sit.a1.f22621632.Project_1632.modules.Room;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelManagementSystem {
+    private List<Room> roomList;
+
+    public HotelManagementSystem() {
+        this.roomList = new ArrayList<>();
+    }
+
+    public List<Room> getRoomList() {
+        return roomList;
+    }
+
+}
