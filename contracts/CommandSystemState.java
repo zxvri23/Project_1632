0f6322d9d98d171299fb27149ea490e7e949cea4
zxvri23 <161262364+zxvri23@users.noreply.github.com>
2024-06-21 16:49:46 +0300

    Fixing the commands

--- contracts/CommandSystemState.java
+++ contracts/CommandSystemState.java
@@ -2,11 +2,20 @@ package bg.tu_varna.sit.a1.f22621632.Project_1632.contracts;
 
 import java.io.File;
 
-public interface CommandSystem {
+public interface CommandSystemState {
     void open(File file);
     void close(File file);
     void save();
     void saveAs();
     void help();
     void exit();
+
+    void checkIn();
+    void checkOut();
+    void availability();
+    void find();
+    void findImportant();
+    void report();
+    void unavailability();
+
 }
--- logger/contracts/Logger.java
+++
@@ -1,7 +0,0 @@
-package bg.tu_varna.sit.a1.f22621632.Project_1632.logger.contracts;
-
-import java.io.IOException;
-
-public interface Logger {
-    void log(String message) throws IOException;
-}
